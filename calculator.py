5bcf1cce9d19c9fbeec4bca068876b21804c6cfb
= <shsshirh18@gmail.com>
2026-09-18 16:09:55 +0530

    Added the Multiplication and Division

--- calculator.py
+++ calculator.py
@@ -5,6 +5,8 @@ b = float(input("Enter second number: "))
 
 print("1. Addition")
 print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
 
 choice = input("Enter your choice (1-4): ")
 
@@ -14,6 +16,11 @@ if choice == "1":
 elif choice == "2":
     print("Subtraction =", a - b)
 
+elif choice == "3":
+    print("Multiplication =", a * b)
+
+elif choice == "4":
+    print("Division =",a / b)
 
 else:
     print("Invalid choice")
